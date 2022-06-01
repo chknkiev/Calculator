fd2052aa9ea1fe6a7b126248e25fd305013cc0f2
Bryan <suprnewb@gmail.com>
2022-06-01 17:07:38 +1000

    Complete code

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.button-container');
 const display = document.querySelector('.display');
 
+
+const operate = (n1, operator, n2) => {
+    let result = '';
+    let firstNum = parseFloat(n1);
+    let secondNum = parseFloat(n2);
+    if(operator === 'add') {
+        result = firstNum + secondNum;
+    } else if(operator === 'subtract') {
+        result = firstNum - secondNum;
+    } else if(operator === 'multiply') {
+        result = firstNum * secondNum;
+    } else if(operator === 'divide') {
+        result = firstNum / secondNum;
+    }
+    result = Math.round(result * 10000) / 10000;
+    return result;
+}
+
+
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
@@ -10,13 +29,18 @@ keys.addEventListener('click', e => {
         const displayedNum = display.textContent;
         const previousKeyType = calculator.dataset.previousKeyType;
         
+
         if(!action) {
-            if(displayedNum === '0' || previousKeyType === 'operator') {
+            if(displayedNum === '0' ||
+            previousKeyType === 'operator' ||
+            previousKeyType === 'operate') {
                 display.textContent = keyContent;
+                calculator.dataset.previousKeyType = 'number';
             } else {
                 display.textContent = displayedNum + keyContent;
             }
         };
+
 
         if(action === 'decimal') {
             if(!displayedNum.includes('.')) {
@@ -27,42 +51,56 @@ keys.addEventListener('click', e => {
         calculator.dataset.previousKeyType = 'decimal';
         }
 
+
         if(action === 'add' ||
         action === 'subtract' ||
         action === 'multiply' ||
         action === 'divide') {
-            calculator.dataset.previousKeyType = 'operator';
-            calculator.dataset.firstValue = displayedNum;
-            calculator.dataset.operator = action;
-        }
-
-        const calculate = (n1, operator, n2) => {
-            let result = '';
-            if(operator === 'add') {
-                result = parseFloat(n1) + parseFloat(n2);
-            } else if(operator === 'subtract') {
-                result = parseFloat(n1) - parseFloat(n2);
-            } else if(operator === 'multiply') {
-                result = parseFloat(n1) * parseFloat(n2);
-            } else if(operator === 'divide') {
-                result = parseFloat(n1) / parseFloat(n2);
-            }
-            return result
-        }
-
-        if(action === 'calculate') {
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayedNum;
-            calculator.dataset.previousKeyType = 'calculate';
-
-            display.textContent = calculate(firstValue, operator, secondValue);
+            if(firstValue &&
+                operator &&
+                previousKeyType !== 'operator') {
+                const operateValue = operate(firstValue, operator, secondValue);
+                display.textContent = operateValue;
+                calculator.dataset.firstValue = operateValue;
+            } else {
+                calculator.dataset.firstValue = displayedNum;
+            }
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.operator = action;
+            key.style.backgroundColor='coral'
         }
+        
+
+        if(action === 'equals') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+            calculator.dataset.previousKeyType = 'operate';
+            
+            if(calculator.dataset.operator === 'divide' &&
+            displayedNum === '0') {
+                alert('Not going to happen!');
+            }
+            
+            if(firstValue) {
+                display.textContent = operate(firstValue, operator, secondValue);
+            }
+            
+            const opBackground = document.getElementsByClassName('operator');
+            for (let i = 0; i < opBackground.length; i++) {
+                opBackground[i].style.backgroundColor = 'lightsalmon';
+            }
+        }
+
 
         if(action === 'clear') {
                 display.textContent = 0;
                 calculator.dataset.previousKeyType = 'clear';
         }
+
 
         if(action === 'all-clear') {
             calculator.dataset.firstValue = '';
@@ -72,6 +110,7 @@ keys.addEventListener('click', e => {
             display.textContent = 0;
         }
     
+
         if(action === 'bg') {
             let letters = "0123456789ABCDEF";
             let color = "#";
@@ -80,6 +119,7 @@ keys.addEventListener('click', e => {
             }
             display.style.backgroundColor = color;
         }
+
 
         if(action === 'font') {
             let letters = "0123456789ABCDEF";
@@ -91,6 +131,3 @@ keys.addEventListener('click', e => {
         }
     } 
 });
-
-
-
